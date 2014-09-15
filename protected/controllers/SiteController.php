15a53f0a65b616214f00c48a7df39f0203a65d40
aneesh.anand <anand.aneesh@hotmail.com>
2014-09-15 17:06:45 +0530

    testing forget password page

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -530,11 +530,8 @@ class SiteController extends Controller
 				$user_id = $model->get_user_id($model->attributes['email']);			
 				$token = $token."user".$user_id;
 				$key = "forget password posly";
-				//echo $encrypted_token = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $token, MCRYPT_MODE_CBC, md5(md5($key))));
-				echo $iv =base64_encode( mcrypt_encrypt( MCRYPT_RIJNDAEL_256, md5( $key ),
-    $token, MCRYPT_MODE_CBC, md5( md5( $key ) ) ) );;
-				exit;
-				//echo $encrypted_string = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $token, MCRYPT_MODE_CBC, $iv);
+				$key = md5($key);
+				echo $encrypted_token = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $token, MCRYPT_MODE_CBC, md5($key)));
 				exit;
 				//$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encrypted_token), MCRYPT_MODE_CBC, md5(md5($key))), "\0"); 
 				$user_token = urlencode($encrypted_token);
